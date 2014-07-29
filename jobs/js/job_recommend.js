04048c3d90761ba619de3cca1563b5e36a1e6935
black85114 <black85114@gmail.com>
2014-07-29 19:49:14 +0800

    Recommend Page Update , Pic rename

--- jobs/js/job_recommend.js
+++ jobs/js/job_recommend.js
@@ -1,47 +1,72 @@
 		$(document).ready(function(){
 			$("table").hide();	//隱藏Table
 			$("#choose").hide().fadeIn('slow');
-			
-			
-			//usr_menu
-			$.getJSON("data/recommendation/resumelist.json", function(data){
-				var userList = '<ul>'
-				for(var i=1; i <= data.resumes.length; i++)
-					userList += '<li>user_' + ((i<10)? '0'+ i : i) + '</li>';
-				userList += '</ul>';
-				$("#usr_menu").append(userList);
-			});
-			
-			//點擊選單
+					
+			/**點擊選單*/
 			var isClick = false;	//按鈕是否被啟動
 			$("#choose").on('click',function(){				
 				$("#usr_menu").slideToggle("fast");				
 				if(isClick){
 					isClick = false;
+					$("#choose").removeAttr('style');
 				}
 				else{
 					isClick = true;
 					$("#choose").attr("style","-moz-box-shadow:5px 5px 5px #999 inset;-webkit-box-shadow:5px 5px 5px #999 inset;box-shadow:5px 5px 5px #999 inset;");
-				}
-					
+				}				
 			});
 			
-			//點擊使用者
-			
+							
+			/**點擊List	*/
+			var listType = '';
+			var listPath = '';			
 			$("#usr_menu").on('click', 'li', function(){	//click後才搜尋 #usr_menu 的li
-				$("#usr_menu").slideUp("fast");
-				$("#bg_info").hide();
 				var clickUser = $(this).text();
-				$("#choose").text( clickUser );
-				$("#choose").removeAttr("style");
-				var num = clickUser.substring(clickUser.length -2);
-				loadResume(parseInt(num) - 1);
-				console.log(parseInt(num) - 1);
-				isClick = false;
+				if(clickUser == 'Recommend by Job' || clickUser == 'Recommend by Resume'){
+				
+					//選擇Recommend By "Job" or "Resume"
+					if($(this).text() == 'Recommend by Job'){
+					listType = 'Job';
+					listPath = "data/recommendation/joblist.json";
+					$("#userId").text('Job ID');
+					$("#icon").html("<img src='images/"+listType+".png' />");
+					}
+					else{
+						listType = 'Resume';	
+						$("#userId").text('User ID');	
+						listPath = "data/recommendation/resumelist.json";
+						$("#icon").html("<img src='images/"+listType+".png' />");					
+					}				
+					//usr_menu
+					$.getJSON( listPath , function(data){
+						$("#usr_menu").html('');		
+						var userList = '<ul><li>back</li>'
+						for(var i=1; i <= data.resumes.length; i++)
+							userList += '<li>'+listType+'_' + ((i<10)? '0'+ i : i) + '</li>';
+						userList += '</ul>';
+						$("#usr_menu").append(userList);
+					});
+				}
+				
+				//選擇List
+				else{		
+					if(clickUser == 'back'){
+						$("#usr_menu").html("");
+						$("#usr_menu").html("<ul><li>Recommend by Job</li><li>Recommend by Resume</li></ul>");
+					}
+					else{
+						$("#choose").text( clickUser );
+						var num = clickUser.substring(clickUser.length -2);
+						loadResume(parseInt(num) - 1);
+						console.log(clickUser.substring(0,clickUser.length-3));
+						isClick = false;
+					}
+				}
 			});
 				
-			//Show推薦清單
+			/**Show推薦清單*/
 			function loadResume(uid){
+			
 				
 				//讀取resumelist檔取出使用者
 				$.getJSON("data/recommendation/resumelist.json", function(data1){
@@ -49,7 +74,8 @@
 					doc += data1.resumes[uid].id_no;
 					doc += ".json";
 					//console.log(doc);
-					$("#userId").text("User ID : " + data1.resumes[uid].id_no).append("<a class='hint--bottom' data-hint='"+data1.resumes[uid].words+"' >");
+					$("#icon").html("<a href='"+data1.resumes[uid].referer+"' target='_blank'><img src='images/"+listType+".png' /></a>");
+					$("#userId").html("<a class='hint--bottom' data-hint='"+data1.resumes[uid].words+"' >User ID : " + data1.resumes[uid].id_no + "</a>")
 					console.log(data1.resumes[uid].id_no);
 					$.getJSON(doc, function(data){
 						var info = '';
@@ -72,14 +98,18 @@
 							info += "<td class='rank'><div class='tbg1'><div class='tbg1' style='background-color:#CCAB6A;width:";
 							info += similar +"%;'><span class='ranking'>"+(i + 1)+"</span >";
 							info += "<span class='result' style='left:50px;'>"+ similar +"</span></div></div></td>";
-							info += "<td class='rank'><a href="+arr[i].url+"  target='_blank'><div class='tbg2'><span class='job'>";
+							info += "<td class='rank'><a  class='hint--bottom' data-hint='"+arr[i].words+"'href="+arr[i].url;
+							info += "  target='_blank'><div class='tbg2'><span class='job'>";
 							info += arr[i].title;
 							info += "</span ><span class='com'>"+arr[i].company+"</span></div></a></td>";
-							info += "<td><a class='hint--bottom' data-hint='"+arr[i].words+"' href='clusters.html?0"+ (arr[i].group<10?'0'+arr[i].group:arr[i].group) +"'  target='_blank'>";
+							info += "<td><a href='clusters.html?0"+ (arr[i].group<10?'0'+arr[i].group:arr[i].group) +"'  target='_blank'>";
 							info += "<img src='images/skill_clustering_g.gif'/></a><td/>";
 							info += "</tr>";
 						}
+						$("#bg_info").hide();
 						$("#tags").html("");
+						$("#usr_menu").slideUp("fast");
+						$("#choose").removeAttr('style');						
 						
 						if(arr.length == 0){
 							console.log('ZERO');
